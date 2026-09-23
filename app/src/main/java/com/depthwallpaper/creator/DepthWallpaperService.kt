2d592e04c4b74555ccd02f5f39470a555418900d
Bixi88 <bix2013@libero.it>
2026-09-23 14:24:08 +0200

    Add files via upload

--- app/src/main/java/com/depthwallpaper/creator/DepthWallpaperService.kt
+++ app/src/main/java/com/depthwallpaper/creator/DepthWallpaperService.kt
@@ -7,6 +7,8 @@ import android.content.IntentFilter
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
